b1855e27a51bdfcd30ec25ecf7fd4b78370db815
Batey Josue <josuebatey19@gmail.com>
2022-03-12 10:16:28 +0200

    Decimal to Binary Convertion func: decToBin(number)

--- me.py
+++ me.py
@@ -7,9 +7,24 @@ def binToDec(number):
         
     return dec
 
+def decToBin(number):
+    binary = []
+    quot = 1
+    while (quot != 0):
+        if number // 2 != 0:
+            quot = number // 2
+            binary.append(number % 2)
+            number = quot
+        else:
+            binary.append(number % 2)
+            quot = 0
+    # print (quot, number)
+    return binary.reverse()
+
 def Main():
     print("Begin:")
-    print(binToDec(11111))
+    print(binToDec(100000))
+    print(decToBin(32))
     print("End:")
 
 if __name__ == '__main__':
