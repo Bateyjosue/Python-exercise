1d0651a510b7a6e729d50ac00a9e820f5d48b5eb
Batey Josue <josuebatey19@gmail.com>
2022-04-23 09:32:44 +0200

    Update file name and add smallest function

---
+++ lagestAndsmall_number.py
@@ -0,0 +1,20 @@
+def largestNumber(num):
+    large = num[0]
+    for n in num:
+        if n > large:
+            large = n
+    return large
+
+def smallestNumber(num):
+    small = num[0]
+    for n in num:
+        if small > n:
+            small = n
+    return small
+
+def Main():
+    print(f'Large: {largestNumber([3, 41, 12, 9, 74, 15])}')
+    print(f'Small: {smallestNumber([3, 41, 12, 9, 74, 15])}')
+
+if __name__ == '__main__':
+    Main()
